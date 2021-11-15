dda7ff40e59421a0720d30ebd672fc9a8b3517ac
d.matros <d.matros@outlook.com>
2021-11-15 17:08:37 +0300

    Добавьте файлы проекта.

---
+++ Lab9WpfApp/MyCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab8WpfApp
+{
+    class MyCommands
+    {
+        public static RoutedUICommand Exit { get; set; }
+        public static RoutedCommand Bond { get; set; }
+        public static RoutedCommand Italic { get; set; }
+        public static RoutedCommand Underline { get; set; }
+        public static RoutedCommand Black { get; set; }
+        public static RoutedCommand Red { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(MyCommands), inputs);
+            Bond = new RoutedCommand("Bond", typeof(MyCommands));
+            Italic = new RoutedCommand("Italic", typeof(MyCommands));
+            Underline = new RoutedCommand("Underline", typeof(MyCommands));
+            Black = new RoutedCommand("Black", typeof(MyCommands));
+            Red = new RoutedCommand("Red", typeof(MyCommands));
+        }
+    }
+}
